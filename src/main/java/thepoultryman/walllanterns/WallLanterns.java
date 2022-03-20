346e5ffc4af8f6fbacc40534188c3cc73f9e3650
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-03-20 01:27:17 -0400

    Remove unnecessary initializer

--- src/main/java/thepoultryman/walllanterns/WallLanterns.java
+++ src/main/java/thepoultryman/walllanterns/WallLanterns.java
@@ -1,15 +1,9 @@
 package thepoultryman.walllanterns;
 
-import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class WallLanterns implements ModInitializer {
+public class WallLanterns {
 	public static final String MOD_ID = "walllanterns";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-	@Override
-	public void onInitialize() {
-		LOGGER.info("Putting lanterns on walls.");
-	}
 }
