7b4c98f7610681ff4889dbd5b33c35971e3feb56
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-05-10 21:47:51 -0400

    Add built-in resource pack for model fix

--- src/main/java/thepoultryman/walllanterns/WallLanterns.java
+++ src/main/java/thepoultryman/walllanterns/WallLanterns.java
@@ -1,9 +1,22 @@
 package thepoultryman.walllanterns;
 
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class WallLanterns {
+public class WallLanterns implements ModInitializer {
 	public static final String MOD_ID = "walllanterns";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
+	@Override
+	public void onInitialize() {
+		FabricLoader.getInstance().getModContainer(MOD_ID).ifPresent(modContainer -> {
+			ResourceManagerHelper.registerBuiltinResourcePack(new Identifier(MOD_ID, "secretroomscompat"), modContainer, ResourcePackActivationType.NORMAL);
+		});
+	}
 }
