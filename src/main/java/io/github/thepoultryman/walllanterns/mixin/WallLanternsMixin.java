b23420a4d1675c785cea6ed637b0ee767fc3f85a
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-07-31 14:12:19 -0400

    Optimize imports

--- src/main/java/io/github/thepoultryman/walllanterns/mixin/WallLanternsMixin.java
+++ src/main/java/io/github/thepoultryman/walllanterns/mixin/WallLanternsMixin.java
@@ -13,7 +13,6 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
-import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
 import net.minecraft.world.WorldView;
 import org.jetbrains.annotations.Nullable;
