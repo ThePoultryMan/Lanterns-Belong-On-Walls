72246aa5de317fa69c926c8e0d4850f0f9b3346a
Xanthian <xanneh@hotmail.com>
2022-09-08 11:11:43 +0100

    variant_lantern compat

--- src/main/java/io/github/thepoultryman/walllanterns/WallLanterns.java
+++ src/main/java/io/github/thepoultryman/walllanterns/WallLanterns.java
@@ -14,7 +14,7 @@ public class WallLanterns implements ModInitializer {
 	public static final String MOD_ID = "walllanterns";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	private static final List<String> COMPAT_MODS = List.of("oxidized", "byg", "charm");
+	private static final List<String> COMPAT_MODS = List.of("oxidized", "byg", "charm", "variant_lanterns");
 
 	@Override
 	public void onInitialize() {
