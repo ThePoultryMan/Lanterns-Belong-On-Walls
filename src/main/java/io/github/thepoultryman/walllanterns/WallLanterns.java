e119b4708d10ae65729cf2882a3c57f0c6d65a77
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2024-04-01 18:16:57 -0400

    Switch to entrypoint api

--- src/main/java/io/github/thepoultryman/walllanterns/WallLanterns.java
+++ src/main/java/io/github/thepoultryman/walllanterns/WallLanterns.java
@@ -1,18 +1,11 @@
 package io.github.thepoultryman.walllanterns;
 
-import io.github.thepoultryman.walllanterns.api.Config;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
-import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.state.property.Properties;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.math.Direction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import virtuoel.statement.api.StateRefresher;
 import virtuoel.statement.api.StatementApi;
-import virtuoel.statement.util.RegistryUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,27 +18,9 @@ public class WallLanterns implements ModInitializer, StatementApi {
 
 	@Override
 	public void onInitialize() {
-		for (Identifier lanternIdentifier : Config.INSTANCE.getVanillaTypeLanterns()) {
-			RegistryUtils.getOrEmpty(RegistryUtils.BLOCK_REGISTRY, lanternIdentifier).ifPresent(
-					lantern -> StateRefresher.INSTANCE.addBlockProperty(lantern, Properties.FACING, Direction.UP)
-			);
+		for (WallLanternsEntrypoint entrypoint : FabricLoader.getInstance().getEntrypoints(MOD_ID, WallLanternsEntrypoint.class)) {
+			entrypoint.patchLanterns();
 		}
-
-		FabricLoader.getInstance().getModContainer(MOD_ID).ifPresent(modContainer -> {
-			boolean activateCompat = false;
-
-			for (String modId : Config.getSupportedMods()) {
-				if (FabricLoader.getInstance().isModLoaded(modId)) {
-					activateCompat = true;
-				}
-			}
-
-			if (activateCompat) {
-				ResourceManagerHelper.registerBuiltinResourcePack(new Identifier(MOD_ID, "moddedlanternscompat"),
-						modContainer, ResourcePackActivationType.ALWAYS_ENABLED);
-			}
-		});
-
 		StateRefresher.INSTANCE.reorderBlockStates();
 	}
 }
